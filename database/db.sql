009c9a9facbafe8af7a0e4944f1f7087bbf8711c
Lin-JCG <joshuacarrasco99@gmail.com>
2021-10-20 07:17:28 -0500

    Fininalizado

--- database/db.sql
+++ database/db.sql
@@ -11,3 +11,14 @@ CREATE TABLE Personas(
 	Mes VARCHAR(10),
 	Monto DECIMAL (5,2)
 );
+
+INSERT INTO Personas (Persona, Mes, Monto) VALUES
+	('Jameson', 'Enero', 125.2),
+	('Cesar', 'Febrero', 35.5),
+	('Jorge', 'Enero', 30.1),
+	('Cesar', 'Marzo', 55.8),
+	('Jorge', 'Abril', 25.0),
+	('Jameson', 'Abril', 30.1),
+	('Jameson', 'Enero', 55.8),
+	('Jorge', 'Febrero', 25.0),
+	('Cesar', 'Enero', 55.8) ;
